dff7d1385850e0e44092b93b03cce7bd391bc171
tnantaki <tinyhave@gmail.com>
2025-10-30 22:13:41 +0700

    front-end - contact page

---
+++ frontend/src/routes/(footers)/contact/InputContact.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		type?: string;
+		placeholder: string;
+		value: string;
+	}
+
+	let { title, type = 'text', placeholder, value = $bindable() }: Props = $props();
+</script>
+
+<label class="flex flex-col">
+	<p class="text-base font-medium leading-normal pb-2">{title}</p>
+	<input class="input-contact form-input resize-none h-14" {placeholder} {type} bind:value />
+</label>
+
---
+++ frontend/src/routes/(footers)/contact/data.ts
@@ -0,0 +1,22 @@
+import { Mail, MapPin, Phone } from '@lucide/svelte';
+
+export type Contact = (typeof contacts)[0];
+export const contacts = [
+	{
+		title: 'email',
+		value: 'contact@thebookstore.com',
+		icon: Mail
+	},
+	{ title: 'phone', value: '+1 (234) 567-890', icon: Phone },
+	{
+		title: 'location_on',
+		value: '123 Bookworm Lane, Readington, BK 12345',
+		icon: MapPin
+	}
+];
+
+export const map = {
+	alt: 'A map showing the location of The Bookstore.',
+	locaton: 'Readington',
+	src: '/images/contact/map.png'
+};
