492194a7292e8110926511f00fe4656a60ec35c9
tnantaki <tinyhave@gmail.com>
2025-10-30 19:50:23 +0700

    front-end - add about page

---
+++ frontend/src/routes/(footers)/about/data.ts
@@ -0,0 +1,39 @@
+export const image_banner = {
+	alt: 'A cozy, well-lit library with floor-to-ceiling bookshelves',
+	src: '/images/about/about00.png'
+};
+
+export const images_gallery = [
+	{
+		alt: 'A stack of old books on a wooden table.',
+		src: '/images/about/about01.png'
+	},
+	{
+		alt: 'An open book with its pages fluttering in the wind.',
+		src: '/images/about/about02.png'
+	},
+	{
+		alt: 'Interior of a modern bookstore with many shelves.',
+		src: '/images/about/about03.png'
+	},
+	{
+		alt: 'A person holding a book, with a blurred background.',
+		src: '/images/about/about04.png'
+	},
+	{
+		alt: 'A person reading a book in a cozy chair by a window.',
+		src: '/images/about/about05.png'
+	},
+	{
+		alt: 'Rows of colorful books on a library shelf.',
+		src: '/images/about/about06.png'
+	},
+	{
+		alt: 'A coffee cup next to an open book on a wooden table.',
+		src: '/images/about/about07.png'
+	},
+	{
+		alt: 'A child reading a book in a sunlit room.',
+		src: '/images/about/about08.png'
+	}
+];
