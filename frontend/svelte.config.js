9a8f653b67c08f5f180bf02247458070745fe29a
tnantaki <tinyhave@gmail.com>
2026-01-08 19:13:56 +0700

    frontend build output

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			out: 'public', // for running on vercel
+			out: 'build',
 			precompress: false,
 			envPrefix: '',
 			polyfill: false
